eea737e781d146399edc3bcc14a07881be135aa0
OrangeMoonOrange <55967922+OrangeMoonOrange@users.noreply.github.com>
2020-10-15 23:54:57 +0800

    添加坦克静止的处理，只要四个键都没有按下去，坦克就是静止的

--- src/main/java/com/truekai/tank/Tank.java
+++ src/main/java/com/truekai/tank/Tank.java
@@ -11,11 +11,20 @@ public class Tank {
     private int x, y;
     private Dir dir = Dir.DOWN;
     private static final int SPEED = 5;
+    private boolean moving = false;
 
     public Tank(int x, int y, Dir dir) {
         this.x = x;
         this.y = y;
         this.dir = dir;
+    }
+
+    public boolean isMoving() {
+        return moving;
+    }
+
+    public void setMoving(boolean moving) {
+        this.moving = moving;
     }
 
     public Dir getDir() {
@@ -31,6 +40,11 @@ public class Tank {
         g.setColor(Color.YELLOW);
         g.fillRect(x, y, 50, 50);
         g.setColor(c);
+        move();
+    }
+
+    private void move() {
+        if(!moving) return;
         switch (dir) {
             case LEFT:
                 x -= SPEED;
