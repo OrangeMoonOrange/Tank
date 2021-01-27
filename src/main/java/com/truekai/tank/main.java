1442298deb55665d5f791ff9397cd769bfaa36df
OrangeMoonOrange <truekai@foxmail.com>
2021-01-27 22:44:17 +0800

    注释了敌方坦克，为netty版本做准备

--- src/main/java/com/truekai/tank/main.java
+++ src/main/java/com/truekai/tank/main.java
@@ -12,11 +12,11 @@ public class main {
     public static void main(String[] args) throws InterruptedException {
         TankFrame tr = new TankFrame();
 
-        int initTankCount = Integer.valueOf((String)PropertyMgr.get(Constants.initTankCount));
-        //初始化地方坦克
-        for (int i = 0; i < initTankCount; i++) {
-            tr.tanks.add(new Tank(200 + i * 80, 100, Dir.DOWN, Group.BAD, tr));
-        }
+//        int initTankCount = Integer.valueOf((String)PropertyMgr.get(Constants.initTankCount));
+//        //初始化地方坦克
+//        for (int i = 0; i < initTankCount; i++) {
+//            tr.tanks.add(new Tank(200 + i * 80, 100, Dir.DOWN, Group.BAD, tr));
+//        }
 
         while (true) {
             Thread.sleep(50);
